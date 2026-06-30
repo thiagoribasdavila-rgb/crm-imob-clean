2221cf41c905a63d37ac41a8117b2087f17e7a41
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-30 00:34:14 -0300

    Update header and paragraph text in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 24 }}>
-      <h1>CRM Rodando ✔</h1>
-      <p>Home funcionando</p>
+      <h1>CRM Online ✔</h1>
+      <p>Sistema inicial funcionando</p>
     </div>
   );
 }
