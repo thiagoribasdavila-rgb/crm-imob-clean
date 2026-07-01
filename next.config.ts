606f4089be9b6371e85d81a1f52182d31707e191
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-01 18:37:32 -0300

    Refactor next.config.ts to use CommonJS module

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  experimental: {
+    serverComponentsExternalPackages: ["@supabase/supabase-js"],
+  },
 };
-
-export default nextConfig;
