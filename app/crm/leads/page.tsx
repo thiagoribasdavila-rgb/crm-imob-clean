b9d9e912ad8a269fe488a0f35f11a5c6c7efbcec
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-02 00:17:43 -0300

    Refactor LeadsClient to LeadsPage with links

--- app/crm/leads/page.tsx
+++ app/crm/leads/page.tsx
@@ -1,5 +1,18 @@
-import LeadsClient from "./LeadsClient"
+import Link from "next/link"
 
-export default function Page() {
-  return <LeadsClient />
+export default function LeadsPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>CRM Imobiliário</h1>
+
+      <ul>
+        <li>
+          <Link href="/crm/leads">Leads</Link>
+        </li>
+        <li>
+          <Link href="/crm/leads/new">Novo Lead</Link>
+        </li>
+      </ul>
+    </main>
+  )
 }
