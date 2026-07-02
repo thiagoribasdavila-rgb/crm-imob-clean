af12b0a74f428c20b8cb2a02281a00675ec2b08d
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-01 23:58:13 -0300

    Refactor Page component to use LeadEditClient

--- app/crm/leads/edit/[id]/page.tsx
+++ app/crm/leads/edit/[id]/page.tsx
@@ -1,35 +1,5 @@
-"use client"
+import LeadEditClient from "./LeadEditClient"
 
-import { useEffect, useState } from "react"
-import { useParams } from "next/navigation"
-import { getSupabase } from "@/lib/supabase/client"
-
-export const dynamic = "force-dynamic"
-
-export default function Page() {
-  const params = useParams()
-  const [lead, setLead] = useState(null)
-
-  useEffect(() => {
-    const supabase = getSupabase()
-
-    async function load() {
-      const { data, error } = await supabase
-        .from("leads")
-        .select("*")
-        .eq("id", params.id)
-        .single()
-
-      if (!error) setLead(data)
-    }
-
-    if (params?.id) load()
-  }, [params?.id])
-
-  return (
-    <div>
-      <h1>Lead</h1>
-      <pre>{JSON.stringify(lead, null, 2)}</pre>
-    </div>
-  )
+export default function Page({ params }: { params: { id: string } }) {
+  return <LeadEditClient id={params.id} />
 }
