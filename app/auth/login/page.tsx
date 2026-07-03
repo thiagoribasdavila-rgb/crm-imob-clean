083fff751bc08cc67d6212c6a288e535fc35a61e
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-03 14:26:51 -0300

    Update login header text to 'CRM SaaS Login'

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -20,7 +20,7 @@ export default function Login() {
 
   return (
     <div style={{ padding: 40 }}>
-      <h1>Login Empresa</h1>
+      <h1>CRM SaaS Login</h1>
 
       <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
       <input placeholder="senha" type="password" onChange={(e) => setPassword(e.target.value)} />
