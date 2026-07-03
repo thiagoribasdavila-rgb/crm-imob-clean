7f9a56035fb9a6b9a9d2ad87fab2b0ba2b5485d6
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-03 15:02:01 -0300

    Add Loading component for CRM

---
+++ app/crm/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 14,
+        color: "#666",
+      }}
+    >
+      Carregando CRM...
+    </div>
+  )
+}
