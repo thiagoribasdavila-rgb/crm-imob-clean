9c9dde2c3d12e123d47e534040cfb5317dca0817
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-03 14:19:02 -0300

    Refactor KanbanBoard to use KanbanColumn component

--- components/crm/KanbanBoard.tsx
+++ components/crm/KanbanBoard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import LeadCard from "./LeadCard"
+import KanbanColumn from "./KanbanColumn"
 
 const columns = ["novo", "contato", "proposta", "fechado"]
 
@@ -8,19 +8,12 @@ export default function KanbanBoard({ leads, refresh }: any) {
   return (
     <div style={{ display: "flex", gap: 20 }}>
       {columns.map((status) => (
-        <div key={status} style={{ flex: 1 }}>
-          <h3>{status.toUpperCase()}</h3>
-
-          {leads
-            .filter((l: any) => l.status === status)
-            .map((lead: any) => (
-              <LeadCard
-                key={lead.id}
-                lead={lead}
-                refresh={refresh}
-              />
-            ))}
-        </div>
+        <KanbanColumn
+          key={status}
+          status={status}
+          leads={leads.filter((l: any) => l.status === status)}
+          refresh={refresh}
+        />
       ))}
     </div>
   )
