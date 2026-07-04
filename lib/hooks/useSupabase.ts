d4d5c36f471cd96f1ef44cdcd1c09e2d1419b4d9
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-04 13:53:53 -0300

    Delete lib/hooks directory

--- lib/hooks/useSupabase.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useMemo } from "react";
-import { getSupabase } from "@/lib/supabase/safeClient";
-
-export function useSupabase() {
-  return useMemo(() => getSupabase(), []);
-}
