444ffee3d1782a9a2e2b7ca590377ec7a2a1d470
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:39:37 -0300

    Create ChatPage component for CRM chat interface

---
+++ app/(crm)/chat/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ChatPage() {
+  return (
+    <div>
+      <h1>Chat CRM</h1>
+
+      <div>
+        <p>Mensagens com leads aqui</p>
+      </div>
+    </div>
+  );
+}
