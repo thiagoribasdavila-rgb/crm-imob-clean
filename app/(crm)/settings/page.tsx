2dde30c93ffe78af7d39273b604155a7d4642b5b
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:28:55 -0300

    Create SettingsPage component for CRM settings

---
+++ app/(crm)/settings/page.tsx
@@ -0,0 +1,11 @@
+export default function SettingsPage() {
+  return (
+    <div>
+      <h1>Settings</h1>
+
+      <section>
+        <p>Configurações do CRM</p>
+      </section>
+    </div>
+  );
+}
