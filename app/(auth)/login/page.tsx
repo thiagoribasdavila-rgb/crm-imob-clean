1638c094bd7c7b3c8979eb7cd1324f6a6d807462
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 19:19:24 -0300

    Refactor Login component to LoginPage with form inputs

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,12 @@
-export default function Login() {
-  return <div>Login OK</div>;
+export default function LoginPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login CRM</h1>
+
+      <input placeholder="Email" />
+      <input placeholder="Senha" type="password" />
+
+      <button>Entrar</button>
+    </div>
+  );
 }
