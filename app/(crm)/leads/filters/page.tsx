d384ffd102d944d1950415093d037ac01be653ac
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:33:27 -0300

    Create LeadFiltersPage component for lead filtering

---
+++ app/(crm)/leads/filters/page.tsx
@@ -0,0 +1,11 @@
+export default function LeadFiltersPage() {
+  return (
+    <div>
+      <h1>Filtros de Leads</h1>
+
+      <button>Hot</button>
+      <button>Warm</button>
+      <button>Cold</button>
+    </div>
+  );
+}
