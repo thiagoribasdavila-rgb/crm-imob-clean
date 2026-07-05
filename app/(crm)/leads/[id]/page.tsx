e6705dc812d364dd27b80af486819a1ad9bf6745
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:32:43 -0300

    Refactor EditLead to LeadDetail with async data fetching

--- app/(crm)/leads/[id]/page.tsx
+++ app/(crm)/leads/[id]/page.tsx
@@ -1,25 +1,14 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { LeadService } from "@/lib/services/leads.services";
 
-export default function EditLead({ params }: any) {
+export default async function LeadDetail({ params }: any) {
   const service = new LeadService();
-  const [lead, setLead] = useState<any>(null);
-
-  useEffect(() => {
-    service.getLeadById(params.id).then(setLead);
-  }, []);
+  const lead = await service.getLeadById(params.id);
 
   return (
     <div>
-      <h1>Editar Lead</h1>
-
-      {lead && (
-        <div>
-          <input defaultValue={lead.name} />
-        </div>
-      )}
+      <h1>{lead.name}</h1>
+      <p>Status: {lead.status}</p>
+      <p>Score: {lead.score}</p>
     </div>
   );
 }
