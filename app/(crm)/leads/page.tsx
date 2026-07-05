cea2bbfa6919665b162f7125e28a9d43dede4f96
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:07:18 -0300

    Create LeadsPage component to display leads

---
+++ app/(crm)/leads/page.tsx
@@ -0,0 +1,18 @@
+import { LeadService } from "@/services/lead.service";
+
+export default async function LeadsPage() {
+  const service = new LeadService();
+  const leads = await service.getLeads();
+
+  return (
+    <div>
+      <h1>Leads</h1>
+
+      {leads?.map((lead: any) => (
+        <div key={lead.id}>
+          {lead.name} - {lead.status}
+        </div>
+      ))}
+    </div>
+  );
+}
