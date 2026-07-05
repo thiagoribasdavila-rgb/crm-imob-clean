b390fee8c743485b066a6ba93a0b0d0249f35947
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 20:14:26 -0300

    Rename Dashboard function to DashboardPage and update import

--- app/(crm)/dashboard/page.tsx
+++ app/(crm)/dashboard/page.tsx
@@ -1,13 +1,12 @@
-import { LeadService } from "@/services/lead.service";
+import { LeadService } from "@/lib/services/leads.services";
 
-export default async function Dashboard() {
+export default async function DashboardPage() {
   const service = new LeadService();
   const leads = await service.getLeads();
 
   return (
     <div>
       <h1>CRM Dashboard</h1>
-
       <p>Total leads: {leads?.length}</p>
     </div>
   );
