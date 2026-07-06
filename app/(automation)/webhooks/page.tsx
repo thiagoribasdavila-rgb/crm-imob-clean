68ee7d462c833a529fc242b14d211dfeea11f9f3
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 23:43:54 -0300

    Create WebhooksPage component for automation

---
+++ app/(automation)/webhooks/page.tsx
@@ -0,0 +1,11 @@
+export default function WebhooksPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🌐 Webhooks</h1>
+
+      <p>Recebendo dados de Meta Ads</p>
+      <p>Recebendo leads do site</p>
+      <p>Sincronizando CRM externo</p>
+    </div>
+  );
+}
