e90dfc6467e594c599689c57057942f1defe1278
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:48:59 -0300

    Create ProductSpawner component for product generation

---
+++ app/(meta-economy)/product-spawner/page.tsx
@@ -0,0 +1,15 @@
+export default function ProductSpawner() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🧬 Product Spawner</h1>
+
+      <p>Cria novos produtos automaticamente</p>
+
+      <ul>
+        <li>CRM para investidores</li>
+        <li>CRM para corretores externos</li>
+        <li>Plataforma de leads premium</li>
+      </ul>
+    </div>
+  );
+}
