7f2583d530ad093f62d4df2ca7a66af68feefedc
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:54:35 -0300

    Create CapitalCore component for financial overview

---
+++ app/(economic-system)/capital-core/page.tsx
@@ -0,0 +1,15 @@
+export default function CapitalCore() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>💰 Capital Core</h1>
+
+      <p>Centro financeiro do sistema</p>
+
+      <ul>
+        <li>Lucro total: R$ 1.240.000</li>
+        <li>Capital ativo: R$ 680.000</li>
+        <li>Reservas: R$ 320.000</li>
+      </ul>
+    </div>
+  );
+}
