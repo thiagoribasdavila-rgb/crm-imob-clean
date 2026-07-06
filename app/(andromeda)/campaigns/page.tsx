eb590c7b6d81952bbc3110302c6d600f5d98aab4
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 23:50:14 -0300

    Create CampaignsPage component for campaign listings

---
+++ app/(andromeda)/campaigns/page.tsx
@@ -0,0 +1,13 @@
+export default function CampaignsPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>📣 Campanhas</h1>
+
+      <ul>
+        <li>Campanha Av Paulista - Ativa</li>
+        <li>Campanha Perdizes - Escalando</li>
+        <li>Campanha Investidor - Pausada IA</li>
+      </ul>
+    </div>
+  );
+}
