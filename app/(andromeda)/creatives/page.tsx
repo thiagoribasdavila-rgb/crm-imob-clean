0d4e9244e2e03537fc001b55ddb06ed0fb601d52
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 23:51:01 -0300

    Create CreativesPage component for ad generation

---
+++ app/(andromeda)/creatives/page.tsx
@@ -0,0 +1,11 @@
+export default function CreativesPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🎨 Criativos IA</h1>
+
+      <p>Gerando anúncios automaticamente</p>
+      <p>Teste A/B ativo</p>
+      <p>CTR otimizado por IA</p>
+    </div>
+  );
+}
