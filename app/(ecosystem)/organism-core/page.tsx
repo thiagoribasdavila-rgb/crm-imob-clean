1126cc24f8bd47b53098deebc100d6d47c2296a0
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:43:08 -0300

    Create OrganismCore component with initial content

---
+++ app/(ecosystem)/organism-core/page.tsx
@@ -0,0 +1,15 @@
+export default function OrganismCore() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🌱 Organism Core</h1>
+
+      <p>Sistema se comporta como organismo vivo digital</p>
+
+      <ul>
+        <li>Leads = células</li>
+        <li>Campanhas = órgãos</li>
+        <li>IA = cérebro</li>
+      </ul>
+    </div>
+  );
+}
