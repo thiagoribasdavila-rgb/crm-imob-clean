de2613660c0a40e3ececfe77e458f59e8ff8f14a
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:07:41 -0300

    Create StagesPage component for pipeline stages

---
+++ app/pipeline/stages/page.tsx
@@ -0,0 +1,15 @@
+export default function StagesPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Stages do Pipeline</h1>
+
+      <ul>
+        <li>Lead</li>
+        <li>Contato</li>
+        <li>Visita</li>
+        <li>Proposta</li>
+        <li>Fechado</li>
+      </ul>
+    </div>
+  );
+}
