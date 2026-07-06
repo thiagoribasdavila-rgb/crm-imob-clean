020aba618efd3561921b20078bf5aa85b7bc94fb
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:16:18 -0300

    Add PropertyMatch component for lead matching

---
+++ app/properties/match/page.tsx
@@ -0,0 +1,9 @@
+export default function PropertyMatch() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Matching IA</h1>
+
+      <p>IA conecta leads com imóveis ideais</p>
+    </div>
+  );
+}
