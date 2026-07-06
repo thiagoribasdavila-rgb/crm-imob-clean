8bc7de097b1f91ac035d086d599920de9f65ba36
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:16:54 -0300

    Add PropertiesMap component for property visualization

---
+++ app/properties/map/page.tsx
@@ -0,0 +1,9 @@
+export default function PropertiesMap() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Mapa de Imóveis</h1>
+
+      <p>Visualização geográfica (futuro Google Maps)</p>
+    </div>
+  );
+}
