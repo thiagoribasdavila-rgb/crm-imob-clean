f898b8d525bc48d7ac1e46a8cee644e7332a1081
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:51:04 -0300

    Create MultiCRMManager component for CRM management

---
+++ app/(meta-economy)/multi-crm-manager/page.tsx
@@ -0,0 +1,15 @@
+export default function MultiCRMManager() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🧠 Multi CRM Manager</h1>
+
+      <p>Gerencia múltiplos CRMs simultaneamente</p>
+
+      <ul>
+        <li>CRM imobiliário principal</li>
+        <li>CRM investidores</li>
+        <li>CRM corretores parceiros</li>
+      </ul>
+    </div>
+  );
+}
