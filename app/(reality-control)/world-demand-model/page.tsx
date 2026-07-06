43b8412e239de02bf6c67ae2fef9e202a3130888
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:30:02 -0300

    Add World Demand Model component

---
+++ app/(reality-control)/world-demand-model/page.tsx
@@ -0,0 +1,15 @@
+export default function WorldDemandModel() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🌐 World Demand Model</h1>
+
+      <p>Modelo global de demanda imobiliária</p>
+
+      <ul>
+        <li>Alta demanda em SP urbano</li>
+        <li>Crescimento de aluguel curto prazo</li>
+        <li>Expansão de investidores estrangeiros</li>
+      </ul>
+    </div>
+  );
+}
