6f7b461a761cb4f0f6cdcfe9044f703d0a686ff0
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:23:24 -0300

    Create FutureCityModel component for future city simulation

---
+++ app/(reality-engine)/future-city-model/page.tsx
@@ -0,0 +1,15 @@
+export default function FutureCityModel() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🏗️ Future City Model</h1>
+
+      <p>Cria cidades imobiliárias futuras simuladas</p>
+
+      <ul>
+        <li>Zona Oeste 2030</li>
+        <li>Centro reativado</li>
+        <li>Novos hubs de investimento</li>
+      </ul>
+    </div>
+  );
+}
