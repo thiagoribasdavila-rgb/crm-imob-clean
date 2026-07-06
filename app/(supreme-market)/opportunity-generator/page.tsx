77e139a6e773dbf7c2862dd9b7c2849645567306
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:10:41 -0300

    Create OpportunityGenerator component for business ideas

---
+++ app/(supreme-market)/opportunity-generator/page.tsx
@@ -0,0 +1,15 @@
+export default function OpportunityGenerator() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🚀 Opportunity Generator</h1>
+
+      <p>Cria oportunidades de negócio automaticamente</p>
+
+      <ul>
+        <li>Lançar produto em Perdizes</li>
+        <li>Explorar Airbnb Pinheiros</li>
+        <li>Focar investidores SP centro</li>
+      </ul>
+    </div>
+  );
+}
