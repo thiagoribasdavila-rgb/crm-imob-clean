88cd5760a8de21db42d3f27df4efa1f98236135a
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:44:09 -0300

    Add Self Organization Engine component

---
+++ app/(ecosystem)/self-organization-engine/page.tsx
@@ -0,0 +1,15 @@
+export default function SelfOrganizationEngine() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🔄 Self Organization Engine</h1>
+
+      <p>Reorganiza estrutura automaticamente</p>
+
+      <ul>
+        <li>Muda fluxo de leads</li>
+        <li>Muda prioridades de pipeline</li>
+        <li>Redistribui recursos</li>
+      </ul>
+    </div>
+  );
+}
