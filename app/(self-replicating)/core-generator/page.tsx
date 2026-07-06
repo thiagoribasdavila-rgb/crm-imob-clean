aa2e060f2c292d73b9de9f203fb653e568e0ac80
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:32:08 -0300

    Create CoreGenerator component for module generation

---
+++ app/(self-replicating)/core-generator/page.tsx
@@ -0,0 +1,15 @@
+export default function CoreGenerator() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🧠 Core Generator</h1>
+
+      <p>Gera novos módulos automaticamente</p>
+
+      <ul>
+        <li>Cria CRM modules</li>
+        <li>Cria pipelines novos</li>
+        <li>Cria fluxos de vendas</li>
+      </ul>
+    </div>
+  );
+}
