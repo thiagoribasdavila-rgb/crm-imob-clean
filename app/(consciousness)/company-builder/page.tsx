448838d6a3ec09a4f0c2bef29931172ab8ccd0b9
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:41:26 -0300

    Create CompanyBuilder component for digital business

---
+++ app/(consciousness)/company-builder/page.tsx
@@ -0,0 +1,15 @@
+export default function CompanyBuilder() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🏢 Company Builder</h1>
+
+      <p>Cria novas empresas digitais automaticamente</p>
+
+      <ul>
+        <li>Imobiliária digital B2C</li>
+        <li>Plataforma de leads</li>
+        <li>SaaS de vendas imobiliárias</li>
+      </ul>
+    </div>
+  );
+}
