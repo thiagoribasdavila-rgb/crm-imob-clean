2d5d06ad8f2aea935daf341fde0996044a1e2854
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 14:05:28 -0300

    Delete app/properties directory

--- app/properties/map/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PropertiesMap() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Mapa de Imóveis</h1>
-
-      <p>Visualização geográfica (futuro Google Maps)</p>
-    </div>
-  );
-}
--- app/properties/match/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PropertyMatch() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Matching IA</h1>
-
-      <p>IA conecta leads com imóveis ideais</p>
-    </div>
-  );
-}
