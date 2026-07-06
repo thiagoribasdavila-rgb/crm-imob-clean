464fd9d7d07ff93794543b2c6b8aea3ac9d52b3c
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 23:51:42 -0300

    Create AudiencesPage component with audience list

---
+++ app/(andromeda)/audiences/page.tsx
@@ -0,0 +1,13 @@
+export default function AudiencesPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🎯 Públicos IA</h1>
+
+      <ul>
+        <li>Investidores Alto Padrão</li>
+        <li>Primeira Compra SP</li>
+        <li>Airbnb / Short Stay</li>
+      </ul>
+    </div>
+  );
+}
