b69744e3b77e46ac604eab10fe9c1d9dcebb8065
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 21:49:56 -0300

    Create ImportPage component for lead import

---
+++ app/leads/import/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ImportPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Importar Leads</h1>
+
+      <input type="file" />
+
+      <p>Upload CSV (futuro: IA parsing)</p>
+    </div>
+  );
+}
