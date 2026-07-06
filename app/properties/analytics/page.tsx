470aca45b44d2c9eacf4cb89077af2b36b3250a1
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:18:10 -0300

    Create PropertyAnalytics component for property analytics

---
+++ app/properties/analytics/page.tsx
@@ -0,0 +1,13 @@
+export default function PropertyAnalytics() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Analytics de Imóveis</h1>
+
+      <ul>
+        <li>Imóveis mais visualizados</li>
+        <li>Conversão por imóvel</li>
+        <li>Demanda por região</li>
+      </ul>
+    </div>
+  );
+}
