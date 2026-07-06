8d6e64742b1e9b34c1e84622c424c15aa62c975e
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:58:37 -0300

    Add ExpansionEngine component for automatic growth

---
+++ app/(economic-system)/expansion-engine/page.tsx
@@ -0,0 +1,15 @@
+export default function ExpansionEngine() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🚀 Expansion Engine</h1>
+
+      <p>Cria crescimento automático do sistema</p>
+
+      <ul>
+        <li>Expande campanhas vencedoras</li>
+        <li>Cria novos produtos</li>
+        <li>Abre novos mercados</li>
+      </ul>
+    </div>
+  );
+}
