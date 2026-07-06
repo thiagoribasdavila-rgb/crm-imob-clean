edcec886704dd19aa9597139babbf325070fbbbe
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 21:59:04 -0300

    Create SegmentsPage component for lead segmentation

---
+++ app/leads/segments/page.tsx
@@ -0,0 +1,14 @@
+export default function SegmentsPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Segmentação de Leads</h1>
+
+      <ul>
+        <li>Investidores</li>
+        <li>Primeira compra</li>
+        <li>Alto padrão</li>
+        <li>Airbnb</li>
+      </ul>
+    </div>
+  );
+}
