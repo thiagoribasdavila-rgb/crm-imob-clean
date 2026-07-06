811569b75443a787f8ab05bfb1ed8edf72c559fb
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:30:54 -0300

    Add Strategic Orchestrator component

---
+++ app/(reality-control)/strategic-orchestrator/page.tsx
@@ -0,0 +1,15 @@
+export default function StrategicOrchestrator() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🧠 Strategic Orchestrator</h1>
+
+      <p>Coordena todas decisões globais do sistema</p>
+
+      <ul>
+        <li>Onde investir</li>
+        <li>O que escalar</li>
+        <li>O que pausar</li>
+      </ul>
+    </div>
+  );
+}
