39d8ec8e69c34da670f84e36d13ab8096b95e445
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:57:55 -0300

    Add Portfolio Manager component

---
+++ app/(economic-system)/portfolio-manager/page.tsx
@@ -0,0 +1,15 @@
+export default function PortfolioManager() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>📊 Portfolio Manager</h1>
+
+      <p>Gerencia todas as “empresas internas”</p>
+
+      <ul>
+        <li>CRM imobiliário</li>
+        <li>Plataforma de leads</li>
+        <li>Ads engine</li>
+      </ul>
+    </div>
+  );
+}
