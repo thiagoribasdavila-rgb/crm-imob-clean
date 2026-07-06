eaeced2337d6b5b8190594ed26f35aaaa23775d5
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:32:58 -0300

    Create ModuleFactory component for automatic module creation

---
+++ app/(self-replicating)/module-factory/page.tsx
@@ -0,0 +1,15 @@
+export default function ModuleFactory() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🏭 Module Factory</h1>
+
+      <p>Cria novos módulos dentro do CRM automaticamente</p>
+
+      <ul>
+        <li>Leads module</li>
+        <li>AI module</li>
+        <li>Sales module</li>
+      </ul>
+    </div>
+  );
+}
