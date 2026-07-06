b49857c1fcfb5cb03a494cd29ec2f7ecb579f4ec
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:17:28 -0300

    Create ImportProperties component for file upload

---
+++ app/properties/import/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ImportProperties() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Importar Imóveis</h1>
+
+      <input type="file" />
+
+      <p>Upload CSV ou integração futura com incorporadoras</p>
+    </div>
+  );
+}
