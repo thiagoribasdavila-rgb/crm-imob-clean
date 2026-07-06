55244700fa325137b017e43e148eb7a448806107
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-05 22:09:43 -0300

    Add PipelineHistory component for displaying history

---
+++ app/pipeline/history/page.tsx
@@ -0,0 +1,9 @@
+export default function PipelineHistory() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Histórico do Pipeline</h1>
+
+      <p>Movimentações de leads no funil</p>
+    </div>
+  );
+}
