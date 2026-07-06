a822b15418d46bcb210a99a016eec2c232121eb4
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 02:04:04 -0300

    Add CollectiveLearning component with initial content

---
+++ app/(collective-intelligence)/collective-learning/page.tsx
@@ -0,0 +1,15 @@
+export default function CollectiveLearning() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>📚 Collective Learning</h1>
+
+      <p>Todos os módulos aprendem juntos em tempo real</p>
+
+      <ul>
+        <li>Melhor conversão compartilhada</li>
+        <li>Melhores estratégias replicadas</li>
+        <li>Erros eliminados globalmente</li>
+      </ul>
+    </div>
+  );
+}
