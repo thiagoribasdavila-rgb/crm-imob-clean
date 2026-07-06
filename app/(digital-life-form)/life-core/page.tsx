8a1b538aa7180a9429aa0bcfea7cfa2a12a2c46e
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 01:55:12 -0300

    Add LifeCore component for digital life simulation

---
+++ app/(digital-life-form)/life-core/page.tsx
@@ -0,0 +1,15 @@
+export default function LifeCore() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🧬 Life Core</h1>
+
+      <p>Núcleo que simula “vida digital” dentro do sistema</p>
+
+      <ul>
+        <li>Recebe estímulos do mercado</li>
+        <li>Processa decisões internas</li>
+        <li>Gera respostas automáticas</li>
+      </ul>
+    </div>
+  );
+}
