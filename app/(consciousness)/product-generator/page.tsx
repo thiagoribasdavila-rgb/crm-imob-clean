8619151316be9f632a3da9f8c2eac8b0a97539ae
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-06 00:40:34 -0300

    Create ProductGenerator component for automatic systems

---
+++ app/(consciousness)/product-generator/page.tsx
@@ -0,0 +1,15 @@
+export default function ProductGenerator() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>🏭 Product Generator</h1>
+
+      <p>Cria novos sistemas automaticamente:</p>
+
+      <ul>
+        <li>CRM secundário</li>
+        <li>Portal de imóveis</li>
+        <li>Sistema de leads externo</li>
+      </ul>
+    </div>
+  );
+}
