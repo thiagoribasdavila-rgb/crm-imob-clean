253226cb415461c63d8c3e6a9aa603d2a1a4bb82
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-07 20:07:59 -0300

    Add serverExternalPackages to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+
+  serverExternalPackages: [
+    "prisma"
+  ],
+
 };
 
 export default nextConfig;
