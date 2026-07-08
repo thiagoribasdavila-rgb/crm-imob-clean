98f22fef1102176db4e9f156674fc1bbceeb790c
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 17:15:21 -0300

    Create CRMHeader component for CRM interface

---
+++ components/crm/CRMHeader.tsx
@@ -0,0 +1,32 @@
+export default function CRMHeader(){
+
+
+return (
+
+<div>
+
+<h1 className="
+text-5xl
+font-bold
+">
+
+Atlas Neural CRM
+
+</h1>
+
+
+<p className="
+text-zinc-400
+mt-2
+">
+
+Sistema operacional comercial imobiliário com IA
+
+</p>
+
+
+</div>
+
+)
+
+}
