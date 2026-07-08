23bf7d984ff41e5696dec5d8c8866ec4e8853b66
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 20:09:30 -0300

    Create index.ts to export reasoning modules

---
+++ core/ai/reasoning/index.ts
@@ -0,0 +1,11 @@
+export * from "./DecisionEngine";
+
+export * from "./PredictionEngine";
+
+export * from "./RecommendationEngine";
+
+export * from "./LearningEngine";
+
+export * from "./ReasoningContext";
+
+export * from "./ReasoningResult";
