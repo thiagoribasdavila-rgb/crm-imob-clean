42e88c06ae4db07b4f24a3fc20059d27e182750b
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:20:47 -0300

    Add CustomerLifetime component for customer insights

---
+++ components/crm/customer/intelligence/CustomerLifetime.tsx
@@ -0,0 +1,60 @@
+export default function CustomerLifetime(){
+
+return (
+
+<div className="
+bg-white
+rounded-xl
+p-6
+shadow
+">
+
+
+<h2 className="
+font-bold
+text-xl
+">
+
+♾️ Customer Lifetime
+
+</h2>
+
+
+<div className="
+mt-5
+space-y-3
+">
+
+
+<p>
+Compra atual:
+Apartamento Premium
+</p>
+
+
+<p>
+Próxima oportunidade:
+Upgrade em 24 meses
+</p>
+
+
+<p>
+Potencial indicação:
+Alto
+</p>
+
+
+<p>
+Valor futuro cliente:
+R$ 5M+
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
