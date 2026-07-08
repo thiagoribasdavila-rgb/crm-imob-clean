8ddc299935134bb1cc768b15e76bc614225df588
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:00:00 -0300

    Add CustomerDeals component for displaying deals

---
+++ components/crm/customer/CustomerDeals.tsx
@@ -0,0 +1,34 @@
+export default function CustomerDeals(){
+
+return(
+
+<div className="bg-white p-6 rounded-xl shadow">
+
+<h2 className="font-bold">
+Negociações
+</h2>
+
+
+<p>
+Arvo Paraíso
+</p>
+
+<p>
+Unidade 1204
+</p>
+
+<p>
+R$ 899.000
+</p>
+
+
+<span>
+🟡 Proposta enviada
+</span>
+
+
+</div>
+
+)
+
+}
