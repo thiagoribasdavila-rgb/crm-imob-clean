f528d01944194d36d9d4626d483e48124dd099a5
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 14:48:22 -0300

    Add AIRecommendation component for lead insights

---
+++ components/crm/AIRecommendation.tsx
@@ -0,0 +1,37 @@
+export default function AIRecommendation(){
+
+return (
+
+<div className="
+rounded-xl
+bg-zinc-900
+text-white
+p-6
+">
+
+<h2>
+Atlas AI Insights
+</h2>
+
+
+<p>
+🤖 12 leads possuem alta intenção de compra
+</p>
+
+
+<p>
+📞 8 leads precisam contato hoje
+</p>
+
+
+<p>
+🏠 Melhor produto:
+Arvo Paraíso
+</p>
+
+
+</div>
+
+)
+
+}
