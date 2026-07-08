d4f3ed50ad63286642ff85a61fe4efa3d06e1aab
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 15:02:00 -0300

    Create index.ts to export domain models

---
+++ domain/index.ts
@@ -0,0 +1,17 @@
+export * from "./lead/Lead"
+
+export * from "./lead/LeadStatus"
+
+export * from "./property/Property"
+
+export * from "./property/PropertyType"
+
+export * from "./customer/Customer"
+
+export * from "./customer/CustomerProfile"
+
+export * from "./deal/Deal"
+
+export * from "./deal/DealStatus"
+
+export * from "./campaign/Campaign"
