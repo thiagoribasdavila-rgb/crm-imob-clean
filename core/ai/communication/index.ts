7f1f35f94c3f120dfef2d81b9b83ca5093cd975f
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 20:32:28 -0300

    Create index.ts to export communication modules

---
+++ core/ai/communication/index.ts
@@ -0,0 +1,17 @@
+export * from "./WhatsAppAgent";
+
+export * from "./InstagramAgent";
+
+export * from "./EmailAgent";
+
+export * from "./ConversationManager";
+
+export * from "./SentimentEngine";
+
+export * from "./ChannelRouter";
+
+export * from "./MessageMemory";
+
+export * from "./CommunicationContext";
+
+export * from "./CommunicationResult";
