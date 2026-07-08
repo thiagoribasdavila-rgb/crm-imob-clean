070b3ba40e3903513e5fe573f2dbabd3f56fcd39
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:03:23 -0300

    Add Customer360 component to CustomerPage

--- app/(atlas)/crm/customer/[id]/page.tsx
+++ app/(atlas)/crm/customer/[id]/page.tsx
@@ -1,1 +1,16 @@
+import Customer360 from "@/components/crm/customer/Customer360";
 
+
+export default function CustomerPage(){
+
+return(
+
+<main className="min-h-screen bg-gray-50 p-8">
+
+<Customer360/>
+
+</main>
+
+)
+
+}
