8c0f2a399bce6b252adcbaf481ef77d14e7a4420
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:14:48 -0300

    Add CustomerEmotionAI component for emotion analysis

---
+++ components/crm/customer/intelligence/CustomerEmotionAI.tsx
@@ -0,0 +1,48 @@
+export default function CustomerEmotionAI(){
+
+return (
+
+<div className="
+bg-white
+rounded-xl
+p-6
+shadow
+">
+
+
+<h2 className="font-bold">
+
+❤️ Emotion Intelligence
+
+</h2>
+
+
+<p className="mt-4">
+
+Sentimento:
+Positivo 84%
+
+</p>
+
+
+<p>
+
+Interesse:
+Alto
+
+</p>
+
+
+<p>
+
+Objeção detectada:
+Preço
+
+</p>
+
+
+</div>
+
+)
+
+}
