75a4b2726ca13526ebdab483f926807ba5fc9dec
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 14:55:50 -0300

    Add Property interface for property details

---
+++ domain/property/Property.ts
@@ -0,0 +1,26 @@
+export interface Property {
+
+id:string
+
+name:string
+
+developer:string
+
+location:string
+
+city:string
+
+price:number
+
+area:number
+
+bedrooms:number
+
+parking:number
+
+type:string
+
+available:boolean
+
+
+}
