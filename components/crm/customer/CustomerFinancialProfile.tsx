c1d94a80dd827d0166b85c1699e781f673e25eb3
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:04:34 -0300

    Create CustomerFinancialProfile component

---
+++ components/crm/customer/CustomerFinancialProfile.tsx
@@ -0,0 +1,46 @@
+export default function CustomerFinancialProfile(){
+
+return(
+
+<div className="bg-white rounded-2xl p-6 shadow">
+
+<h2 className="text-xl font-bold">
+Perfil Financeiro IA
+</h2>
+
+
+<div className="mt-5 space-y-3">
+
+
+<p>
+💰 Ticket estimado:
+R$ 1.500.000
+</p>
+
+
+<p>
+🏦 Capacidade financiamento:
+Alta
+</p>
+
+
+<p>
+📈 Potencial investimento:
+R$ 2 milhões
+</p>
+
+
+<p>
+Score financeiro:
+91/100
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
