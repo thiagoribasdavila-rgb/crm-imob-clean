9d31366c98e19b6352de8c8e33c5f44f481433cc
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 14:27:09 -0300

    Create RevenueForecast component for revenue display

---
+++ components/atlas/RevenueForecast.tsx
@@ -0,0 +1,38 @@
+export default function RevenueForecast(){
+
+return (
+
+<div
+className="
+bg-zinc-900
+border
+border-zinc-800
+rounded-xl
+p-6
+">
+
+
+<h2 className="text-xl font-bold">
+Previsão IA
+</h2>
+
+
+<div className="
+text-5xl
+font-bold
+mt-5
+">
+R$ 12.8M
+</div>
+
+
+<p className="text-zinc-400 mt-3">
+Receita provável próximos 90 dias
+</p>
+
+
+</div>
+
+)
+
+}
