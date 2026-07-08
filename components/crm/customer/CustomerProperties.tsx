97955387684b8784381557a13c43f40e1f6a1177
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:00:49 -0300

    Create CustomerProperties component for displaying matches

---
+++ components/crm/customer/CustomerProperties.tsx
@@ -0,0 +1,29 @@
+export default function CustomerProperties(){
+
+return(
+
+<div className="bg-white p-6 rounded-xl shadow">
+
+<h2 className="font-bold">
+Matching IA
+</h2>
+
+
+<p>
+98% Arvo Paraíso
+</p>
+
+<p>
+94% Inside Perdizes
+</p>
+
+<p>
+89% Jardins Residence
+</p>
+
+
+</div>
+
+)
+
+}
