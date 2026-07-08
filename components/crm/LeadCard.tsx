473c698ee9227028fd81dd81081bb836f0234d0d
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 14:46:50 -0300

    Refactor LeadCard component to use lead object

--- components/crm/LeadCard.tsx
+++ components/crm/LeadCard.tsx
@@ -1,66 +1,27 @@
-export default function LeadCard({
-nome,
-produto,
-score,
-status
-}:{
-nome:string
-produto:string
-score:number
-status:string
-}){
+<div>
 
+<h3>{lead.nome}</h3>
 
-return (
-
-<div className="
-bg-zinc-900
-border
-border-zinc-800
-rounded-xl
-p-4
-hover:border-blue-500
-transition
-">
-
-
-<h3 className="
-font-bold
-text-lg
-">
-{nome}
-</h3>
-
-
-<p className="text-zinc-400 text-sm mt-2">
-{produto}
+<p>
+Produto:
+{lead.produto}
 </p>
 
 
-<div className="
-flex
-justify-between
-mt-4
-">
+<div>
 
-
-<span>
-{status}
-</span>
-
-
-<strong className="
-text-green-400
-">
-{score}%
+Score IA:
+<strong>
+{lead.scoreIA}
 </strong>
 
-
 </div>
 
 
+<p>
+Próxima ação:
+{lead.proximaAcao}
+</p>
+
+
 </div>
-
-)
-
-}
