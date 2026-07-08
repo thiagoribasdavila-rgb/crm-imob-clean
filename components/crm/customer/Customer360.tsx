c2deb6cd1ff89d9168abb5ab2cdd4cb8ecb2671d
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 17:49:36 -0300

    Implement Customer360 component with multiple subcomponents

--- components/crm/customer/Customer360.tsx
+++ components/crm/customer/Customer360.tsx
@@ -1,1 +1,54 @@
+import CustomerHeader from "./CustomerHeader";
+import CustomerProfileCard from "./CustomerProfileCard";
+import CustomerScore from "./CustomerScore";
+import CustomerAIInsights from "./CustomerAIInsights";
+import CustomerJourney from "./CustomerJourney";
+import CustomerTimeline from "./CustomerTimeline";
+import CustomerDeals from "./CustomerDeals";
+import CustomerProperties from "./CustomerProperties";
+import CustomerActions from "./CustomerActions";
 
+
+export default function Customer360(){
+
+return (
+
+<div className="space-y-6">
+
+<CustomerHeader/>
+
+<div className="grid grid-cols-3 gap-6">
+
+<div className="col-span-2 space-y-6">
+
+<CustomerProfileCard/>
+
+<CustomerJourney/>
+
+<CustomerTimeline/>
+
+<CustomerDeals/>
+
+</div>
+
+
+<div className="space-y-6">
+
+<CustomerScore/>
+
+<CustomerAIInsights/>
+
+<CustomerProperties/>
+
+<CustomerActions/>
+
+</div>
+
+
+</div>
+
+</div>
+
+)
+
+}
