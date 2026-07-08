b6d7cf03b0560a27262b380c052cd9fd1ac70221
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 20:19:28 -0300

    Add NegotiationAgent class with analyze method

---
+++ core/ai/agent/NegotiationAgent.ts
@@ -0,0 +1,35 @@
+export class NegotiationAgent {
+
+
+
+analyze(
+
+objection:string
+
+){
+
+
+return {
+
+
+objection,
+
+
+suggestion:
+
+"Apresentar benefícios financeiros e diferenciais do empreendimento"
+
+
+};
+
+
+}
+
+
+
+}
+
+
+
+export const negotiationAgent =
+new NegotiationAgent();
