fbaa2572de921d3576940bc77c759a0ece305b68
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 17:02:26 -0300

    Restore and add exports in lead index
    Re-added exports for LeadScoring and LeadIntelligence, and added new exports for LeadMemory, LeadPrediction, LeadRecommendation, LeadBehavior, LeadJourney, LeadAutomation, and LeadAnalytics.

--- domain/lead/index.ts
+++ domain/lead/index.ts
@@ -1,13 +1,27 @@
 export * from "./Lead";
 
-export * from "./LeadScoring";
-
 export * from "./LeadAI";
 
 export * from "./LeadPipeline";
 
-export * from "./LeadIntelligence";
-
 export * from "./LeadRepository";
 
+export * from "./LeadScoring";
+
 export * from "./LeadStatus";
+
+export * from "./LeadIntelligence";
+
+export * from "./LeadMemory";
+
+export * from "./LeadPrediction";
+
+export * from "./LeadRecommendation";
+
+export * from "./LeadBehavior";
+
+export * from "./LeadJourney";
+
+export * from "./LeadAutomation";
+
+export * from "./LeadAnalytics";
