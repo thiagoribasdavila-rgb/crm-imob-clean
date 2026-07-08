9baed3b9a87c47776dc118f0352aafa3730df7c0
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 18:35:24 -0300

    Add CustomerMemoryScore component for displaying score

---
+++ components/crm/customer/intelligence/CustomerMemoryScore.tsx
@@ -0,0 +1,44 @@
+export default function CustomerMemoryScore(){
+
+return (
+
+<div className="
+bg-white
+rounded-xl
+shadow
+p-6
+">
+
+
+<h2 className="
+font-bold
+">
+
+🎯 Memory Score
+
+</h2>
+
+
+<div className="
+text-5xl
+font-bold
+mt-5
+">
+
+92%
+
+</div>
+
+
+<p className="mt-3">
+
+Confiança das informações armazenadas pela IA
+
+</p>
+
+
+</div>
+
+)
+
+}
