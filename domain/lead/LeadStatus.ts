c15562b2dce223913cb43726242ac2f93e66bdc8
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 14:54:05 -0300

    Add LeadStatus enum for lead management

---
+++ domain/lead/LeadStatus.ts
@@ -0,0 +1,17 @@
+export enum LeadStatus {
+
+NOVO="novo",
+
+QUALIFICADO="qualificado",
+
+VISITA="visita",
+
+PROPOSTA="proposta",
+
+NEGOCIACAO="negociacao",
+
+VENDIDO="vendido",
+
+PERDIDO="perdido"
+
+}
