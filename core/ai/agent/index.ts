f1d8aba32e2aedd5ca09f3a142b3c610bd3008e7
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 20:20:32 -0300

    Create index.ts to export agent modules

---
+++ core/ai/agent/index.ts
@@ -0,0 +1,15 @@
+export * from "./SalesAgent";
+
+export * from "./FollowUpAgent";
+
+export * from "./QualificationAgent";
+
+export * from "./NegotiationAgent";
+
+export * from "./PropertyMatchAgent";
+
+export * from "./AgentMemory";
+
+export * from "./AgentContext";
+
+export * from "./AgentResult";
