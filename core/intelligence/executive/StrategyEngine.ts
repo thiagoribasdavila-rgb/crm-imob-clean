920d75b94d2c722dca22ceb6f1329ff632f3fd59
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 10:34:19 -0300

    Implement StrategyEngine class with recommendation method

---
+++ core/intelligence/executive/StrategyEngine.ts
@@ -0,0 +1,35 @@
+export class StrategyEngine {
+
+
+recommend(
+
+data:any
+
+){
+
+
+return {
+
+
+strategy:
+
+"Priorizar leads de maior intenção e ajustar campanhas",
+
+
+confidence:
+
+88
+
+
+};
+
+
+}
+
+
+}
+
+
+
+export const strategyEngine =
+new StrategyEngine();
