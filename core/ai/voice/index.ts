a2122cbf356c22ab7b91b416f3971ed18ee525bb
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 00:14:41 -0300

    Create index.ts to export voice-related modules

---
+++ core/ai/voice/index.ts
@@ -0,0 +1,13 @@
+export * from "./SpeechToText";
+
+export * from "./VoiceAnalyzer";
+
+export * from "./CallSummary";
+
+export * from "./EmotionVoiceEngine";
+
+export * from "./VoiceMemory";
+
+export * from "./AgentVoiceResponse";
+
+export * from "./VoiceQualityScore";
