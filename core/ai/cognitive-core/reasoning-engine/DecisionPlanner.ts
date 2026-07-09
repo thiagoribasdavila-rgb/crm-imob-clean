cbb98b672ea974f2d99eafe39cc0f7705d2da77f
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 20:05:35 -0300

    Add DecisionPlanner class with createPlan method

---
+++ core/ai/cognitive-core/reasoning-engine/DecisionPlanner.ts
@@ -0,0 +1,34 @@
+export class DecisionPlanner {
+
+
+createPlan(goal:string){
+
+
+return {
+
+
+goal,
+
+
+steps:[
+
+"Analyze data",
+
+"Generate options",
+
+"Select best action",
+
+"Execute",
+
+"Measure result"
+
+]
+
+
+};
+
+
+}
+
+
+}
