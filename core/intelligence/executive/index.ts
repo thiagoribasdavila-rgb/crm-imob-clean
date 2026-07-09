9d572f3179b75f3bdbb52dd33ecd9032b2c688bd
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 10:35:31 -0300

    Create index.ts to export executive modules

---
+++ core/intelligence/executive/index.ts
@@ -0,0 +1,13 @@
+export * from "./ExecutiveAI";
+
+export * from "./StrategyEngine";
+
+export * from "./SalesForecastAI";
+
+export * from "./MarketingAI";
+
+export * from "./RiskEngine";
+
+export * from "./OpportunityEngine";
+
+export * from "./ExecutiveInsight";
