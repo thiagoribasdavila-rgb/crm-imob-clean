062cb2655430c595f7a2f9a49085042b34111e61
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 23:44:58 -0300

    Add marketRecommendation function for investment advice

---
+++ core/intelligence/market/MarketRecommendation.ts
@@ -0,0 +1,33 @@
+export function marketRecommendation(
+
+score:number
+
+){
+
+
+if(score > 80){
+
+return {
+
+recommendation:
+
+"Aumentar investimento na região"
+
+};
+
+}
+
+
+
+return {
+
+
+recommendation:
+
+"Monitorar mercado"
+
+
+};
+
+
+}
