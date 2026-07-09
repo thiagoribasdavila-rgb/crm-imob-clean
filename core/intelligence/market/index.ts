9e71de21c56142adc615d7c7912b1164b44e8b10
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 23:45:27 -0300

    Create index.ts to export market-related modules

---
+++ core/intelligence/market/index.ts
@@ -0,0 +1,17 @@
+export * from "./MarketEntity";
+
+export * from "./NeighborhoodIntelligence";
+
+export * from "./PriceEngine";
+
+export * from "./DemandEngine";
+
+export * from "./ValuationEngine";
+
+export * from "./CompetitionEngine";
+
+export * from "./InvestmentScore";
+
+export * from "./MarketPrediction";
+
+export * from "./MarketRecommendation";
