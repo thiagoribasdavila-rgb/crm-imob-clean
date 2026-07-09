ad2f40d6789a5d81d386c05e9b3c231fab86525a
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 00:28:34 -0300

    Create Index.ts to export learning modules

---
+++ core/ai/learning/Index.ts
@@ -0,0 +1,13 @@
+export * from "./LearningEngine";
+
+export * from "./PatternDetector";
+
+export * from "./ConversionAnalyzer";
+
+export * from "./FeedbackLoop";
+
+export * from "./ModelImprovement";
+
+export * from "./LearningMemory";
+
+export * from "./InsightGenerator";
