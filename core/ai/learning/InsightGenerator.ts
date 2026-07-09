8c801dc5df6392daa45f9f209b128e3273b9e0f2
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 00:27:16 -0300

    Add InsightGenerator class for generating insights

---
+++ core/ai/learning/InsightGenerator.ts
@@ -0,0 +1,35 @@
+export class InsightGenerator {
+
+
+generate(
+
+data:any
+
+){
+
+
+return {
+
+
+insight:
+
+"Clientes investidores respondem melhor a argumentos de valorização.",
+
+
+priority:
+
+"high"
+
+
+};
+
+
+}
+
+
+}
+
+
+
+export const insightGenerator =
+new InsightGenerator();
