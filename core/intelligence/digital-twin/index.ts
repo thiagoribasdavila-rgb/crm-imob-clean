1654411c3e83266185eb7ed7776e2d0166829113
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 10:45:14 -0300

    Create index.ts to export digital twin modules

---
+++ core/intelligence/digital-twin/index.ts
@@ -0,0 +1,17 @@
+export * from "./BusinessTwin";
+
+export * from "./ScenarioEngine";
+
+export * from "./SimulationEngine";
+
+export * from "./RevenueSimulator";
+
+export * from "./MarketingSimulator";
+
+export * from "./SalesSimulator";
+
+export * from "./ResourceOptimizer";
+
+export * from "./TwinMemory";
+
+export * from "./SimulationResult";
