8712059e1821f820004ad0593a27e71b668ec2e9
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 11:02:02 -0300

    Implement AlertEngine class with check method

---
+++ core/operating-system/AlertEngine.ts
@@ -0,0 +1,33 @@
+export class AlertEngine {
+
+
+check(
+
+data:any
+
+){
+
+
+return {
+
+
+alerts:[
+
+"Novo lead quente",
+
+"Meta próxima do objetivo"
+
+]
+
+
+};
+
+
+}
+
+
+}
+
+
+export const alertEngine =
+new AlertEngine();
