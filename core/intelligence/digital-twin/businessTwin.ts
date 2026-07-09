a113737ef628a1d2adb6eb7b7c4b4a03a243f735
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 10:38:29 -0300

    Add BusinessTwin interface for digital twin model

---
+++ core/intelligence/digital-twin/businessTwin.ts
@@ -0,0 +1,25 @@
+export interface BusinessTwin {
+
+
+company:string;
+
+
+pipeline:number;
+
+
+monthlyLeads:number;
+
+
+conversionRate:number;
+
+
+salesTeam:number;
+
+
+marketingBudget:number;
+
+
+createdAt:Date;
+
+
+}
