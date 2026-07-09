d8b4943e7e066624f558d35d9e054655c5140455
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 19:02:40 -0300

    Add new exports for intelligence center modules

--- core/operating-system/intelligence-center/index.ts
+++ core/operating-system/intelligence-center/index.ts
@@ -6,3 +6,11 @@ export * from "./MarketRadar";
 export * from "./RiskDetection";
 export * from "./GrowthEngine";
 export * from "./DecisionMemory";
+
+
+export * from "./DecisionSimulator";
+export * from "./AutonomousRecommendationEngine";
+export * from "./CustomerBehaviorIntelligence";
+export * from "./AutonomousPlanningEngine";
+export * from "./KnowledgeMemoryGraph";
+export * from "./AgentManager";
