8c19b8f660d22e8e896050f282178ab00c97414f
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 00:24:39 -0300

    Add ConversionAnalyzer class for sales analysis

---
+++ core/ai/learning/ConversionAnalyzer.ts
@@ -0,0 +1,35 @@
+export class ConversionAnalyzer {
+
+
+analyze(
+
+sales:any[]
+
+){
+
+
+return {
+
+
+conversionRate:
+
+75,
+
+
+bestAction:
+
+"Contato rápido após cadastro"
+
+
+};
+
+
+}
+
+
+}
+
+
+
+export const conversionAnalyzer =
+new ConversionAnalyzer();
