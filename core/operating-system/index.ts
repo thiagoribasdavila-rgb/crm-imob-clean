b5d2f7e0bb1006247ab63fcf38f48eeb3592a8e1
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 11:04:35 -0300

    Create index.ts to export operating system modules

---
+++ core/operating-system/index.ts
@@ -0,0 +1,15 @@
+export * from "./AtlasOS";
+
+export * from "./OperationMonitor";
+
+export * from "./ActionPlanner";
+
+export * from "./TaskGenerator";
+
+export * from "./GoalTracker";
+
+export * from "./AlertEngine";
+
+export * from "./WorkflowEngine";
+
+export * from "./AutonomousDecision";
