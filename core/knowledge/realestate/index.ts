ef4ea1b1caaf295d4308be369fc6c77f52ce29bf
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-08 23:02:23 -0300

    Create index.ts to export real estate modules

---
+++ core/knowledge/realestate/index.ts
@@ -0,0 +1,15 @@
+export * from "./RealEstateEntity";
+
+export * from "./PropertyGraph";
+
+export * from "./NeighborhoodGraph";
+
+export * from "./DeveloperGraph";
+
+export * from "./MarketGraph";
+
+export * from "./BuyerProfileGraph";
+
+export * from "./SimilarityEngine";
+
+export * from "./GraphRecommendation";
