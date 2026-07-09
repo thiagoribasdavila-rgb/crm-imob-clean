f88d48aace1d2a92e1d584164e06b8c0fedcd688
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 19:29:00 -0300

    Create index.ts

---
+++ core/ai/cognitive-core/index.ts
@@ -0,0 +1,19 @@
+export * from "./CognitiveCore";
+
+export * from "./CognitiveState";
+
+export * from "./KnowledgeEngine";
+
+export * from "./PredictionCore";
+
+export * from "./ContextEngine";
+
+export * from "./MemoryOrchestrator";
+
+export * from "./ReasoningCore";
+
+export * from "./StrategyEngine";
+
+export * from "./SimulationEngine";
+
+export * from "./EvolutionEngine";
