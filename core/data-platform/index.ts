68d5bc7144a82df6d1761f687b49b0e3c26e0895
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-09 11:20:58 -0300

    Create index.ts to export data platform modules

---
+++ core/data-platform/index.ts
@@ -0,0 +1,14 @@
+export * from "./events/Event";
+export * from "./events/EventBus";
+export * from "./events/EventStore";
+
+export * from "./analytics/Metric";
+export * from "./analytics/KPIEngine";
+export * from "./analytics/AnalyticsEngine";
+
+export * from "./warehouse/DataEntity";
+export * from "./warehouse/DataRepository";
+export * from "./warehouse/DataLake";
+
+export * from "./realtime/RealtimeEngine";
+export * from "./realtime/StreamProcessor";
