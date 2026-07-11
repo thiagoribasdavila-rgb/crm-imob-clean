e0ebe94fc9485bff75b9256f875f3a59737e4d56
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-11 01:28:58 -0300

    Upgrade Atlas application shell

--- app/(crm)/layout.tsx
+++ app/(crm)/layout.tsx
@@ -1,14 +1,16 @@
 import type { ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
 import SupabaseGuard from "@/components/SupabaseGuard";
+import AtlasTopbar from "@/components/AtlasTopbar";
 
 export default function CRMLayout({ children }: { children: ReactNode }) {
   return (
     <SupabaseGuard>
-      <div className="min-h-screen bg-zinc-950 text-white lg:flex">
+      <div className="atlas-shell">
         <Sidebar />
-        <main className="min-w-0 flex-1 px-4 pb-10 pt-24 sm:px-6 lg:px-8 lg:py-8">
-          <div className="mx-auto w-full max-w-[1600px]">{children}</div>
+        <AtlasTopbar />
+        <main className="atlas-main">
+          <div className="atlas-content">{children}</div>
         </main>
       </div>
     </SupabaseGuard>
