0aa4e60e0571619210ceac3901cb942aade9abde
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-12 12:47:59 -0300

    feat: register Atlas AI lead 360 tool

--- lib/ai/tools/index.ts
+++ lib/ai/tools/index.ts
@@ -1,8 +1,14 @@
 import { registerAtlasTool } from "../registry";
 import { getDashboardMetricsTool } from "./dashboard";
 import { searchLeadsTool } from "./leads";
+import { getLead360Tool } from "./lead360";
 
 registerAtlasTool(getDashboardMetricsTool);
 registerAtlasTool(searchLeadsTool);
+registerAtlasTool(getLead360Tool);
 
-export { getDashboardMetricsTool, searchLeadsTool };
+export {
+  getDashboardMetricsTool,
+  searchLeadsTool,
+  getLead360Tool,
+};
