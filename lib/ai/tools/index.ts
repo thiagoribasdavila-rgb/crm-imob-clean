21e9ac8198c8982ce9ac5fbaeb7a7b4979f80325
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-07-12 12:50:02 -0300

    feat: register Atlas AI attention tool

--- lib/ai/tools/index.ts
+++ lib/ai/tools/index.ts
@@ -2,13 +2,16 @@ import { registerAtlasTool } from "../registry";
 import { getDashboardMetricsTool } from "./dashboard";
 import { searchLeadsTool } from "./leads";
 import { getLead360Tool } from "./lead360";
+import { listAttentionLeadsTool } from "./attention";
 
 registerAtlasTool(getDashboardMetricsTool);
 registerAtlasTool(searchLeadsTool);
 registerAtlasTool(getLead360Tool);
+registerAtlasTool(listAttentionLeadsTool);
 
 export {
   getDashboardMetricsTool,
   searchLeadsTool,
   getLead360Tool,
+  listAttentionLeadsTool,
 };
